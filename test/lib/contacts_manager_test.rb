66d4e529ced585606c2d8b96b88a3b9a87cf0c50
Ciaran Archer <carcher@zendesk.com>
2013-02-22 11:10:05 -0800

    Added fixture conversion to parameters.

--- test/lib/contacts_manager_test.rb
+++ test/lib/contacts_manager_test.rb
@@ -5,23 +5,23 @@ class ContactsManagerTest < ActiveSupport::TestCase
   describe ContactsManager do
     setup do
       @contact = contacts(:dan)
-      @params = ActionController::Parameters.new({:contact => {
-          :first_name => @contact.first_name,
-          :last_name => @contact.last_name
-        }
-      })
+
+      contact_params = Hash[*JSON.load(@contact.to_json).map{ |k, v| [k.to_sym, v] }.flatten]
+
+      @params = ActionController::Parameters.new({:contact => contact_params})
     end
 
     it 'returns the set of parameters for a contract' do
       expected = {
-        "first_name" => @contact.first_name,
-        "last_name" => @contact.last_name
+        'first_name' => @contact.first_name,
+        'last_name' => @contact.last_name,
+        'email' => @contact.email,
+        'notes' => @contact.notes
       }
       assert_equal ContactsManager.params_permitted(@params), expected
     end
 
     it 'should persist a contact' do
-
       assert_equal ContactsManager.persist(@contact), true
     end
   end
